d1c973d3755dcd943b1031f2cf560d868a02ce77
Lucas Tuozzo <lucastuozzo@gmail.com>
2017-07-22 19:25:08 -0300

    Add files via upload

---
+++ Poteitos/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE.html>
+<html>
+  <head>
+    <title>Poteito's</title>
+    <script src="js/main.js" language="javascript"></script>
+    <link href="css/style.css" rel="stylesheet" type="text/css" media="all">
+  </head>
+  <body>
+    <div id="menus">
+      <div id="topMenu">
+        <div class="botones">Contacto</div>
+        <div class="botones">Eventos</div>
+        <div class="botones">Mercancía</div>
+        <div class="botones">Inicio</div>
+      </div>
+      <div id="socialMenu">
+        <div class="circulo">
+          <div class="socialButton"><img class="circulo" src="imagenes/fbicon.jpg"></div>
+        </div>
+        <div class="circulo">
+          <div class="socialButton"><img class="circulo" src="imagenes/igicon.jpg"></div>
+        </div>
+      </div>
+    </div>
+    <div id="inicio">
+      <div id="logo"><img class="circulo" src="imagenes/logo.jpg"></div>
+    </div>
+    <div id="mercancia">
+    </div>
+    <div id="eventos">
+    </div>
+    <div id="contacto">
+    </div>
+  </body>
+</html>
